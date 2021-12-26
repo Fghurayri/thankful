769255e3896ac05ffc6ba43bc0a91284ca74b57d
Faisal Alghurayri <fghurayri@gmail.com>
2021-12-26 14:18:23 +0300

    add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -10,7 +10,9 @@ import type { MetaFunction } from "remix";
 import styles from "./tailwind.css";
 
 export function links() {
-  return [{ rel: "stylesheet", href: styles }];
+  return [
+    { rel: "stylesheet", href: styles },
+  ];
 }
 
 export const meta: MetaFunction = () => {
@@ -23,6 +25,10 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
         <Meta />
         <Links />
       </head>
